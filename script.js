32f6291e1eea838d1a0c60c3a576d890d667bee8
Lucas Henrique Silva <159385729+lucashensil@users.noreply.github.com>
2024-05-09 08:50:39 -0300

    add error handling

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 import { returnsArrays } from "./src/calculosInvestimentos";
 
 const calculateButton = document.getElementById("calculate-results");
+const form = document.getElementById("investment-form");
 
-function renderProgression() {
+function renderProgression(event) {
+  event.preventDefault();
+  if (document.querySelector(".error")) {
+    return;
+  }
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
   const contribution = Number(
-    document.getElementById("addiotional-contribution").value
+    document.getElementById("addiotional-contribution").value.replace(",", ".")
   );
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnArray = returnsArrays(
     startingAmount,
@@ -24,7 +33,40 @@ function renderProgression() {
     returnRatePeriod
   );
 
-  console.log(returnArray)
+  console.log(returnArray);
 }
 
-calculateButton.addEventListener('click', renderProgression)
+function validateInput(event) {
+  if (event.target.value === "") {
+    return;
+  }
+  const parentElement = event.target.parentElement;
+  const grandParentElement = event.target.parentElement.parentElement;
+  const inputValue = event.target.value.replace(",", ".");
+  if (
+    !parentElement.classList.contains("error") &&
+    (isNaN(inputValue) || Number(inputValue) <= 0)
+  ) {
+    const textElement = document.createElement("p");
+    textElement.classList.add("text-red-500");
+    textElement.innerText = "Insira um valor numérico maior do que zero";
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(textElement);
+  } else if (
+    parentElement.classList.contains("error") &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove("error");
+    grandParentElement.querySelector("p").remove();
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+
+calculateButton.addEventListener("click", renderProgression);
